2658a55d2e01436efe47066b90ea95de1c0953a8
Daniel Delegacz <ddelegacz@gmail.com>
2024-08-19 21:07:11 +0200

    small fixes to mainPage

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -5,8 +5,9 @@ function HomeContent({ heading }) {
         <div className={classes.content}>
             <h1 className={classes.center}>{heading}</h1>
             <ul className={classes.list}>
-                <li>
-                    <h3 className={classes.center}>Zarządzaj swoją dietą w ramach wygodnego interfejsu podzielonego na tygodnie</h3>
+                <li className={classes.weekDays}>
+                    <h3 className={classes.center}>Zarządzaj swoją dietą w ramach wygodnego interfejsu podzielonego na
+                        tygodnie</h3>
                     <div className={classes.center}>
                         <img className={classes.image} src={'week_days_view.png'} alt="week_days_view"/>
                     </div>
@@ -18,15 +19,15 @@ function HomeContent({ heading }) {
                     </div>
                 </li>
                 <li>
-                    <h3 className={classes.center}>Podglądaj na bieżąco statystyki per posiłek, dzień i tydzień</h3>
-                    <div className={classes.center}>
-                        <img className={classes.image} src={'week_stats_view.png'} alt="week_days_view"/>
-                    </div>
-                </li>
-                <li>
                     <h3 className={classes.center}>Dodawaj własne produkty</h3>
                     <div className={classes.center}>
                         <img className={classes.image} src={'new_product_view.png'} alt="week_days_view"/>
+                    </div>
+                </li>
+                <li>
+                    <h3 className={classes.center}>Podglądaj na bieżąco statystyki per posiłek, dzień i tydzień</h3>
+                    <div className={classes.center}>
+                        <img className={classes.image} src={'week_stats_view.png'} alt="week_days_view"/>
                     </div>
                 </li>
                 <li>
